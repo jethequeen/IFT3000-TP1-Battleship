c4252cdba1bb6f08536bb4c949b1b07b3c665f49
Jeremie Queenton <jeremie.queenton.1@ulaval.ca>
2025-05-29 22:24:59 -0400

    Implémentation de la fonction createShip, et une helper fonction generateCoords!

--- Battleship/Battleship.Core/Ship.fs
+++ Battleship/Battleship.Core/Ship.fs
@@ -21,11 +21,34 @@ module Ship =
 
     (* ------- À COMPLÉTER ------- *)
     (* --- Nouvelles fonctions --- *)
+    
+    let rec generateCoords (remaining: int) (coordsList : Coord List)
+                           (facing: Direction) (center: Coord) : Coord List =
+        match remaining with
+        | 0 -> coordsList
+        | _ ->
+            let (x,y) = center
+            let nextCoord =
+                match facing with
+                | North -> (x, y-1)
+                | South -> (x, y+1)
+                | East  -> (x+1, y)
+                | West -> (x-1, y)
+                    
+            generateCoords (remaining-1) (nextCoord :: coordsList) facing center
 
     let createShip (center: Coord) (facing: Direction) (name: Name) : Ship =
-        (* ------- À COMPLÉTER ------- *)
-        (* ----- Implémentation ------ *)
-        { Coords = []; Center = (0, 0); Facing = North; Name = Spy }
+        let length =
+            match name with
+            | Spy -> 2
+            | PatrolBoat -> 2
+            | Destroyer -> 3
+            | Submarine -> 4
+            | Cruiser -> 4
+            | AircraftCarrier -> 5
+        
+        let coords = generateCoords length [] facing center
+        { Coords = coords; Center = center; Facing = facing; Name = name }
 
     let getPerimeter (ship: Ship) (dims: Dims) : Coord list =
         (* ------- À COMPLÉTER ------- *)
